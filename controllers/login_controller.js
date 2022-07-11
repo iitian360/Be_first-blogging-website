30d3d3db6dcb1ae685e3763b44572d4e1e831b24
iitian360 <sp48840@gmail.com>
2022-07-11 14:53:20 +0530

    login veryfication

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -1,7 +1,38 @@
+import blog_modal from '../model/blog_intro.js';
 // import {join} from 'path';
 const myLogin=(req,res)=>{
     // res.sendFile(join(process.cwd(),'view','login.html'));
     res.render('login',{'title':'Login here'});
+}
+
+const verify= async(req,res)=>{
+    try {
+        const {email, password}=req.body;
+        const result= await blog_modal.findOne({email:email});
+        // console.log(result);
+        if(result!=null){
+            if(result.email==email && result.password==password){
+                res.send(`hey ${result.name} welcome to befirst family
+                this is your dashboard.`)
+            }
+            else{
+                res.send('ohh there are some error!');
+            }
+        }
+        else{
+            res.send('you are not a registerd user')
+        }
+
+        
+        
+        
+        
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
 
 const myRegister=(req,res)=>{
@@ -9,4 +40,6 @@ const myRegister=(req,res)=>{
     res.render('registration',{'title':'Login here'});
 }
 
-export {myLogin,myRegister};
+
+
+export {myLogin,myRegister, verify};
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { myLogin,myRegister } from '../controllers/login_controller.js';
+import { myLogin,myRegister,verify } from '../controllers/login_controller.js';
 const router=express.Router();
 
 router.get('/', myLogin);
+router.post('/',verify)
 router.get('/registration',myRegister);
 
 export default router;
